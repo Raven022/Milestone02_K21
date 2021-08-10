8db1554adfd0a6ffcee8ca3030a7999e4149466b
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2021-08-10 19:55:32 +0700

    Nambah EditSekolah page dan request list dict iqi

--- src/src/App.js
+++ src/src/App.js
@@ -8,6 +8,7 @@ import Navbars from "./components/Navbars";
 import Home from "./components/Home";
 import About from "./components/About";
 import LoginPage from "./components/loginPage";
+import EditSekolah from "./components/EditSekolah";
 
 function App() {
   // List Akun
@@ -62,7 +63,7 @@ function App() {
           <h1>Profile Sekolah</h1>
         </Route>
         <Route exact path="/edit_sekolah">
-          <h1>Edit Sekolah</h1>
+          <EditSekolah />
         </Route>
       </Switch>
     </Router>
@@ -70,3 +71,41 @@ function App() {
 }
 // Home, List Sekolah, Login, About, Profile Sekolah, Edit Sekolah
 export default App;
+
+// ########## Request iqi #########
+// const dataSekolah = [{
+//   email_1: "1@1.com",
+//   data: {
+//     image: './../img/sekolah_1.svg',
+//     namaSekolah: "Sekolah Dasar Isla Terpadu Al-Huda",
+//     alamatSekolah: "Jl. P. Bangka Raya No.1, RT.004/RW.016, Aren Jaya, Kec. Bekasi Tim., Kota Bks, Jawa Barat 17111",
+//     kontak: <p>Telp : (021) 82696958 <br />E-mail : sdital.huda@sch.ac.id</p>,
+//     kebutuhan: "Membutuhkan buku matematika dan IPA kelas 2-4 SD, Membutuhkan buku IPS kelas 5 sd",
+//     row_1: <tr>
+//       <td>Matematika Kelas 3 SD</td>
+//       <td>40</td>
+//       <td>Belum Terpenuhi</td>
+//     </tr>,
+//     row_2: <tr>
+//       <td>IPA Terpadu Kelas 4 SD</td>
+//       <td>36</td>
+//       <td>Belum Terpenuhi</td>
+//     </tr>
+//   }
+// },
+// {
+//   email_2: "2@2.com",
+//   data: {
+//     image: './../ img / sekolah_2.jpg',
+//     namaSekolah: "Sekolah Dasar YPWKS 2",
+//     alamatSekolah: "Komplek Krakatau Steel, Jl. Gabus No.13, Kebondalem, Kec. Purwakarta, Kota Cilegon, Banten 42433",
+//     kontak: <p>Telp : (0254) 385351</p>,
+//     kebutuhan: "Membutuhkan buku Bahasa Inggris kelas 4 SD",
+//     row_1: <tr>
+//       <td>Bahasa Inggris Kelas 4 SD</td>
+//       <td>90</td>
+//       <td>Belum Terpenuhi</td>
+//     </tr>
+//   }
+// }
+// ]
---
+++ src/src/components/EditSekolah.js
@@ -0,0 +1,61 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./EditSekolah.css";
+import { Container, Row, Table, Col, Button } from 'react-bootstrap';
+// import sekolah_1 from './../img/sekolah_1.svg';
+function EditSekolah() {
+
+    return (
+        <div className="bg">
+            <Container>
+                <Row>
+                    <Col lg={true}>
+                        <img src={require('./../img/sekolah_1.svg').default} class="school"></img>
+                    </Col>
+                    <Col lg={true}>
+                        <div class="right">
+                            <h3>Nama Sekolah</h3>
+                            <p>Sekolah Dasar Isla Terpadu Al-Huda</p>
+                            <h3>Alamat Sekolah</h3>
+                            <p>Jl. P. Bangka Raya No.1, RT.004/RW.016, Aren Jaya, Kec. Bekasi Tim., Kota Bks, Jawa Barat 17111</p>
+                            <h3>Kontak</h3>
+                            <div> <p>Telp : (021) 82696958 <br />E-mail : sdital.huda@sch.ac.id</p></div>
+                            <h3>Kebutuhan</h3>
+                            <p>Membutuhkan buku matematika dan IPA kelas 2-4 SD, Membutuhkan buku IPS kelas 5 sd</p>
+                            <h3>Riwayat Donasi</h3>
+                            <Table striped bordered hover class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Nama Buku</th>
+                                        <th>Jumlah</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Matematika Kelas 3 SD</td>
+                                        <td>40</td>
+                                        <td>Belum Terpenuhi</td>
+                                    </tr>
+                                    <tr>
+                                        <td>IPA Terpadu Kelas 4 SD</td>
+                                        <td>36</td>
+                                        <td>Belum Terpenuhi</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </div>
+                        <Button>Edit Profile</Button>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div >
+    );
+}
+
+export default EditSekolah;
+
+
+
+
+
